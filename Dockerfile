72109e669924f01529ba897e838c848c1bc236eb
Andrew Theken <andrew@attr.io>
2023-07-14 15:26:36 +0000

    A working container + build.

--- Dockerfile
+++ Dockerfile
@@ -9,14 +9,19 @@ RUN vite build
 
 FROM alpine:3.18
 RUN apk add -u nodejs npm rclone restic bash
-ENV SHELL=/bin/bash
-COPY --from=build /data/ui/build /app
+COPY --from=build /data/build /app
 WORKDIR /app
 COPY ./src/package.json ./
 COPY ./src/package-lock.json ./
+# it's not clear to me why these are necessary, the default nodejs handling should
+# gracefully stop the process
+RUN echo '; process.on("SIGINT", () => { process.exit(0) });' >> ./index.js
+RUN echo '; process.on("SIGTERM", () => { process.exit(0) });' >> ./index.js
+RUN echo '; process.on("SIGKILL", () => { process.exit(0) });' >> ./index.js
 RUN npm i --omit dev
 ENV NODE_ENV=production
 ENV RESTIC_CACHE_DIR=/restic-cache
+ENV PORT=8888
 VOLUME /restic-cache
 VOLUME /configs
-ENTRYPOINT "/usr/bin/node ."
+ENTRYPOINT ["/usr/bin/node", "/app"]
--- src/svelte.config.js
+++ src/svelte.config.js
@@ -1,6 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
+import adapterNode from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+
+let prod = process.env.NODE_ENV === "production"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,12 +11,9 @@ const config = {
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 	kit: {
-		//outDir: prodBuild ? './dist' :  '.svelte-kit' ,
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter:prod ? adapterNode() : adapter(),
 	}
+	
 };
 
 export default config;
